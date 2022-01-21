ac8481bdf6ae180618e9e4157b11cf3846561e90
Cedric Lindigkeit <cedric@lindigkeit.eu>
2022-01-21 10:23:33 +0100

    Initial upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module eu.lindigkeit.tellme {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires eu.hansolo.tilesfx;
+
+    opens eu.lindigkeit.tellme to javafx.fxml;
+    exports eu.lindigkeit.tellme;
+}
